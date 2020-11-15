999875c3cd42e141d88cc5495be92fa4a13c0d3f
popodopolis <jameseroni@pop-os.localdomain>
2020-11-14 19:26:04 -0600

    initial

---
+++ leads/urls.py
@@ -0,0 +1,9 @@
+#use router from rest framework instead of standard urls
+
+from rest_framework import routers
+from .api import LeadViewSet
+
+router = routers.DefaultRouter()
+router.register('api/leads', LeadViewSet, 'leads')
+
+urlpatterns = router.urls
